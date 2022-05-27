aa7a846f79b8a158e4d103f81ea87e73bd41f5dc
Thinh Tran <48557139+ThinhTranA@users.noreply.github.com>
2022-05-28 06:50:59 +0800

    Initial Commit

---
+++ XcodeAssetsIconGenerator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  XcodeAssetsIconGenerator
+//
+//  Created by Thinh Tran on 28/5/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ XcodeAssetsIconGenerator/XcodeAssetsIconGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeAssetsIconGeneratorApp.swift
+//  XcodeAssetsIconGenerator
+//
+//  Created by Thinh Tran on 28/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct XcodeAssetsIconGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
